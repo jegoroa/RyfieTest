10f4b3631ab7466c83b6fe82012e53e6d3d83bfe
jegoroa <75141849+jegoroa@users.noreply.github.com>
2022-04-03 17:06:47 +0300

    Add files via upload
    добавили заготовку 2го экрана

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ from PyQt5.QtWidgets import QVBoxLayout, QHBoxLayout
 
 app = QApplication([])
 start_win = QWidget()
-
 start_vertical = QVBoxLayout()
 
 start_title = QLabel("ПРИВЕТСТВУЕМ В ТЕСТЕ РУФЬЕ")
@@ -17,4 +16,23 @@ start_vertical.addWidget(start_button)
 
 start_win.setLayout(start_vertical)
 start_win.show()
+
+test_win = QWidget()
+test_vertical = QVBoxLayout()
+
+age_input = QLineEdit()
+fio_input = QLineEdit()
+
+test_vertical.addWidget(QLabel("Введите ФИО"))
+test_vertical.addWidget(fio_input)
+test_vertical.addWidget(QLabel("Введите возраст"))
+test_vertical.addWidget(age_input)
+
+test_win.setLayout(test_vertical)
+
+def show_test():
+    start_win.hide()
+    test_win.show()
+
+start_button.clicked.connect(show_test)
 app.exec()
