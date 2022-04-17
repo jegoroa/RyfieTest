27ed8015570ffbcd743e873613452bcb8c98c797
ibo <ibo@lib33.ru>
2022-04-17 17:31:12 +0300

    добавили таймеры

--- windows.py
+++ windows.py
@@ -1,4 +1,4 @@
-from PyQt5.QtCore import Qt
+from PyQt5.QtCore import Qt, QTimer, QTime
 from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QPushButton, QLineEdit
 from PyQt5.QtWidgets import QVBoxLayout, QHBoxLayout
 from settings import *
@@ -26,7 +26,59 @@ class TestWin(QWidget):
         super().__init__()
         self.setWindowTitle("Тестирование")
         self.initUI()
+
+        self.timer1 = QTimer()
+        self.timer1.timeout.connect(self.first_timer_change)
+        self.first_test_btn.clicked.connect(self.start_first_timer)
+
+        self.timer2 = QTimer()
+        self.timer2.timeout.connect(self.second_timer_change)
+        self.squat_btn.clicked.connect(self.start_second_timer)
+
+        self.timer3 = QTimer()
+        self.timer3.timeout.connect(self.third_timer_change)
+        self.final_test_btn.clicked.connect(self.start_third_timer)
+
         self.show()
+
+    def start_first_timer(self):
+        self.time = QTime(0,0,15)
+        self.timer1.start(1000)
+
+    def first_timer_change(self):
+        self.time = self.time.addSecs(-1)
+        self.timer_label.setText(self.time.toString("hh:mm:ss"))
+        if self.time.second() == 0:
+            self.timer1.stop()
+
+    def start_second_timer(self):
+        self.time = QTime(0,0,45)
+        self.timer2.start(1000)
+
+    def second_timer_change(self):
+        self.time = self.time.addSecs(-1)
+        self.timer_label.setText(self.time.toString("hh:mm:ss"))
+        if self.time.second() == 0:
+            self.timer2.stop()
+
+    def start_third_timer(self):
+        self.timer_label.setStyleSheet('color:green;font-size:72px')
+        self.time = QTime(0,0,45)
+        self.timer3.start(1000)
+
+    def third_timer_change(self):
+        self.time = self.time.addSecs(-1)
+        self.timer_label.setText(self.time.toString("hh:mm:ss"))
+        
+        if self.time.second() == 30:
+            self.timer_label.setStyleSheet('color:black;font-size:72px')
+
+        if self.time.second() == 15:
+            self.timer_label.setStyleSheet('color:green;font-size:72px')
+
+        if self.time.second() == 0:
+            self.timer_label.setStyleSheet('color:black;font-size:72px')
+            self.timer3.stop()
 
     def initUI(self):
         
@@ -42,28 +94,33 @@ class TestWin(QWidget):
 
         test_vertical.addWidget(QLabel(TEXT_HELP1))
 
-        first_test_btn = QPushButton("начать первый тест")
-        test_vertical.addWidget(first_test_btn)
+        self.first_test_btn = QPushButton("начать первый тест")
+        test_vertical.addWidget(self.first_test_btn)
 
-        timer = QLabel("00:00:15")
-        test_vertical.addWidget(timer)
+        self.timer_label = QLabel("00:00:15")
+        self.timer_label.setStyleSheet("font-size:72px")
+        test_vertical.addWidget(self.timer_label)
+
+        test_vertical.addWidget(QLabel("Впишите пульс сюда"))
 
         first_puls_input = QLineEdit("0")
         test_vertical.addWidget(first_puls_input)
 
         test_vertical.addWidget(QLabel(TEXT_HELP2))
 
-        squat_btn = QPushButton("начать приседания")
-        test_vertical.addWidget(squat_btn)
+        self.squat_btn = QPushButton("начать приседания")
+        test_vertical.addWidget(self.squat_btn)
 
         test_vertical.addWidget(QLabel(TEXT_HELP3))
 
-        final_test_btn = QPushButton("начать финальный тест")
-        test_vertical.addWidget(final_test_btn)
+        self.final_test_btn = QPushButton("начать финальный тест")
+        test_vertical.addWidget(self.final_test_btn)
 
+        test_vertical.addWidget(QLabel("Впишите пульс за первые 15 сек. сюда"))
         second_puls_input = QLineEdit("0")
         test_vertical.addWidget(second_puls_input)
 
+        test_vertical.addWidget(QLabel("Впишите пульс за последние 15 сек. сюда"))
         third_puls_input = QLineEdit("0")
         test_vertical.addWidget(third_puls_input)
 
